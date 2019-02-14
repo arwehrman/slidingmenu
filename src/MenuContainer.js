7316e5f1be52d9d56bf61a7d56179b1b3eebddf6
Amanda Wehrman <arwehrman@gmail.com>
2019-02-13 19:13:26 -0500

    added event handler for Menu click

--- src/MenuContainer.js
+++ src/MenuContainer.js
@@ -1,6 +1,29 @@
 import React, { Component } from "react";
 
 class MenuContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: false
+    };
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  handleMouseDown(e) {
+    this.toggleMenu();
+    console.log("clicked");
+    e.stopPropagation();
+  }
+
+
+  toggleMenu() {
+    this.setState({
+      visible: !this.state.visible
+    });
+  }
+
   render(){
     return (
       <div>
